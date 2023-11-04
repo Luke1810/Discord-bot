30f9bf62d75a61c3b9249974e3413a0a54a5be7b
Luke1810 <elijoel.eh@gmail.com>
2023-11-04 22:50:56 +0100

    marin

--- source/events/marin.js
+++ source/events/marin.js
@@ -1,5 +1,6 @@
-/**@param {import('discord.js').Message} message */
+/** @param {import('discord.js').Message} message */
 module.exports = function messageCreate(message) {
-
-if(message.author === '1170477409250852884') return message.reply('Hallo marin wie gehts dir')
-}
+    if (message.author.id === '774960022157393932') {
+      message.reply('Hallo marin wie gehts dir');
+    }
+  };
