37b911e5b85fc5b92b57e200cdccdb1bcea1d752
Luke1810 <elijoel.eh@gmail.com>
2023-11-11 18:37:54 +0100

    working months, endlich

--- source/commands/others/until.js
+++ source/commands/others/until.js
@@ -26,11 +26,13 @@ module.exports = {
     const timeDifference = targetDate - currentDate - 3600000;
 
     const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365.25));
-    const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24));
+    const months = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * (365.25 / 12)));
+    const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * (365.25 / 12))) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    
 
-    return message.reply(`${years} Jahre, ${days} Tage, ${hours} Stunden, ${minutes} Minuten, ${seconds} Sekunden`);
+    return message.reply(`${years} Jahre, ${months} Monate, ${days} Tage, ${hours} Stunden, ${minutes} Minuten, ${seconds} Sekunden`);
   }
 };
