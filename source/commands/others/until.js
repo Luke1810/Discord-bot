f7e17b2a6c11524de63aa580e941d80224f6a97d
Luke1810 <elijoel.eh@gmail.com>
2023-11-11 18:23:27 +0100

    months add

--- source/commands/others/until.js
+++ source/commands/others/until.js
@@ -26,11 +26,13 @@ module.exports = {
     const timeDifference = targetDate - currentDate - 3600000;
 
     const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365.25));
+    const daysInYear = Math.floor(timeDifference % (1000 * 60 * 60 * 24 * 365.25));
+    const months = Math.floor(daysInYear / 30); 
     const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    return message.reply(`Es sind noch ${years} Jahre, ${days} Tage, ${hours} Stunden, ${minutes} Minuten und ${seconds} Sekunden bis zum ${targetDateStr}. :alarm_clock:`);
+    return message.reply(`${years} Jahre, ${months} Monate, ${days} Tage, ${hours} Stunden, ${minutes} Minuten, ${seconds} Sekunden`);
   }
 };
