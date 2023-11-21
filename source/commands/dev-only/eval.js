118cc37b60e77c4131228e25e7ac480d55212655
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2023-11-21 12:15:48 +0100

    add eval

---
+++ source/commands/dev-only/eval.js
@@ -0,0 +1,26 @@
+// Credits to https://github.com/Mephisto5558/Teufelsbot/blob/main/Commands/Owner-Only/eval.js
+const
+  vars = ['__dirname', '__filename', 'exports', 'module', 'require'],
+  BoundAsyncFunction = (async function () { }).constructor.bind(null, ...vars),
+  BoundFunction = Function.bind(null, ...vars);
+
+module.exports = {
+  name: 'eval',
+  description: 'Führe JavaScript Code mit dem Bot aus',
+
+  /**@param {import('discord.js').Message} message*/
+  prefixRun: async function (message) {
+    content = args.join(' ');
+    if (!content) return;
+
+    try {
+      await (content.includes('await') ? new BoundAsyncFunction(content) : new BoundFunction(content)).call(message, __dirname, __filename, exports, module, require, lang);
+      await message.reply('Code:\n```js\n' + content + '\n```\nwurde fehlerlos ausgeführt.');
+    }
+    catch (err) {
+      message.reply('Code:\n```js\n' + content + '\n```\nwurde mit dem folgenden Fehler ausgeführt:\n```\n' + `${err.name}: ${err.message}\n` + '```');
+    }
+
+    return console.debug(`evaluated command '${content}'`);
+  }
+};
